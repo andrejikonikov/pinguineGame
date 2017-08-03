0b7d31f362a35ae59a1336e6141a5e200e8f3b24
Andrej <andrej@localhost.localdomain>
2017-08-03 13:08:47 +0300

    game controller

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 $(function() {
-    console.log( "ready!" );
 
     let pinguin = {
         body : $(".pinguin"),
@@ -24,21 +23,105 @@ $(function() {
                 }, 300 );
             }, 300);
 
+        },
+
+        move : function () {
+            console.log("move...");
+            this.body.animate({ //jump
+                top: "40vh",
+                height: "16.6vw"
+            }, 300 );
+            this.body.animate({ //fall
+                top: "54.6vh"
+            }, 500 );
         }
     };
-    console.log(pinguin);
 
 
-    $(document).keydown(function(e) {
-        switch(e.which) {
-            case 38: // up
-            console.log("up");
-            pinguin.jump();
-            break;
 
-            default: return; // exit this handler for other keys
+    let gameController = {
+
+        sceneMaxH : 600,
+        sceneMinH : 240,
+        sceneMaxW : 800,
+        sceneMinW : 320,
+        sky : $(".sky"),
+        ice : $(".ice"),
+        glacier : $(".glacier"),
+
+        setExtremeValues : function (h, w) {
+            this.sky.height(h * 0.6);
+            this.ice.height(h * 0.3);
+            this.glacier.height(h * 0.1);
+        },
+
+
+        startGame : function () {
+            console.log("startGame...");
+            while (true) {
+                setTimeout( function () {
+                    pinguin.move();
+                } , 100);
+            }
+
+        },
+
+        recalculateSizes : function (height, width) {
+            console.log("counting dimensions...");
+            console.log(height, width);
+
+            if ((height < this.sceneMinH) && (width < this.sceneMinW)) {
+                this.setExtremeValues(this.sceneMinH, this.sceneMinW);
+            }
+
+            if ((height > this.sceneMaxH) && (width > this.sceneMaxW)) {
+                this.setExtremeValues(this.sceneMaxH, this.sceneMaxW);
+            } else {
+                this.sky.height(height * 0.6);
+                this.ice.height(height * 0.3);
+                this.glacier.height(height * 0.1);
+            }
         }
-        e.preventDefault(); // prevent the default action (scroll / move caret)
+    };
+
+
+
+
+    // main program :D
+
+    gameController.recalculateSizes($(".scene").height(), $(".scene").width());
+    gameController.startGame();
+
+
+    console.log( "ready!" );
+
+
+    $(window).resize(function(){
+        gameController.recalculateSizes($(".scene").height(), $(".scene").width());
     });
 
+
+
 });
+
+
+
+
+
+//
+//
+
+// console.log(pinguin);
+//
+//
+// $(document).keydown(function(e) {
+//     switch(e.which) {
+//         case 38: // up
+//         console.log("up");
+//         pinguin.jump();
+//         break;
+//
+//         default: return; // exit this handler for other keys
+//     }
+//     e.preventDefault(); // prevent the default action (scroll / move caret)
+// });
